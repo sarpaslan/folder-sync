ffd756e2e918582794ae1e4381f924e10d0cab30
sarpaslan <scharplover@gmail.com>
2024-03-29 13:04:27 +0300

    .

--- Main.c
+++ Main.c
@@ -1,58 +1,54 @@
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <string.h>
 #include "dirent.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define RED "\033[31m"
 #define RESET "\033[0m"
-void sync_folder(char *f, char *f2);
+
+
+const int FILE_ID_T = 32768;
+const int DIRECTORY_ID_T =  16384;
+
+
+void sync_directories(char *f, char *f2);
 int main(int argc, char **argv)
 {
+
     if (argc != 3)
     {
         printf(RED "Invalid argument count!\n" RESET);
         printf("Usage: format /path/to/folder/ another/path/to/folder\n");
         return 1;
     }
-
-    sync_folder(argv[1], argv[2]);
+    sync_directories(argv[1], argv[2]);
 }
 
-void sync_folder(char *path1, char *path2)
-{
-    DIR *folder1 = opendir(path1);
-    DIR *folder2 = opendir(path2);
-    if (folder1 == NULL || folder2 == NULL)
-    {
-        perror("Unable to read directories");
-        exit(EXIT_FAILURE);
+
+void sync_directories(char *path1, char *path2) {
+    DIR *dir1 = opendir(path1);
+    DIR *dir2 = opendir(path2);
+
+    if (!dir1 || !dir2) {
+        printf("Error: Unable to open directories.\n");
+        exit(1);
     }
-
-    struct dirent *entry;
-    int files = 0;
-    while ((entry = readdir(folder1)))
-    {
-        printf("File %3d: %s\n", files, entry->d_name);
-
-        char target_file[PATH_MAX];
-        snprintf(target_file, sizeof(target_file),
-                 "%s/%s", folder1, entry->d_name);
-
-        if (S_ISDIR(entry->d_type))
-            continue;
-        printf("snprintf: %s\n", target_file);
-        struct stat fileStat;
-        if (stat(target_file, &fileStat) == -1)
-        {
-            printf("Failed to get file status%s", target_file);
-            exit(EXIT_FAILURE);
+    struct dirent *en;
+    while((en=readdir(dir1))!=NULL){
+        struct dirent *rec;
+        if(DIRECTORY_ID_T==en->d_type){
+            if((abs(strcmp(en->d_name, "."))==0 || abs(strcmp(en->d_name,"..")) ==0))
+            {
+                continue;
+            }
+            printf("dir: %s",en->d_name);
         }
-        if (S_ISREG(fileStat.st_mode))
-        {
-            printf("File %s,  last modified time %s", entry->d_name, ctime(&fileStat.st_mtime));
+        else if(FILE_ID_T == en->d_type){
+            printf("file: %d - %s\n",en->d_type,en->d_name);
         }
     }
-    closedir(folder1);
-    closedir(folder2);
+    closedir(dir1);
+    closedir(dir2);
 }
---
+++ run.bat
@@ -0,0 +1,2 @@
+make
+.\Sync.exe SyncFolderSource/ SyncFolderTarget/
